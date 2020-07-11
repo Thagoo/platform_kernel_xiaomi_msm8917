9ad69204020737d6485804d13fc864538a4dabe9
Tyler Nijmeh <tylernij@gmail.com>
2020-07-11 18:02:03 +0000

    schedutil: Hardcode up/down rate limit to 10000 us
    {up_,down_}rate_limit_us / min_sample_time: 0 --> 10000: Only adjust frequencies once per scheduling cycle to reduce jitter or stutter caused by unrealistic frequency scaling.
    
    [ghostrider-reborn]:
     * Taken from ktweak by Tyler Nijmeh (https://github.com/Magisk-Modules-Alt-Repo/ktweak)
     * Hardcode it in kernel so post_boot or qcom perf doesn't overwrite it
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I4036e4e48e2026f510af0def1f478c60c722b9ea

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -582,7 +582,7 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
 
-	tunables->up_rate_limit_us = rate_limit_us;
+	tunables->up_rate_limit_us = 10000;
 
 	list_for_each_entry(sg_policy, &attr_set->policy_list, tunables_hook) {
 		sg_policy->up_rate_delay_ns = rate_limit_us * NSEC_PER_USEC;
@@ -602,7 +602,7 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
 
-	tunables->down_rate_limit_us = rate_limit_us;
+	tunables->down_rate_limit_us = 10000;
 
 	list_for_each_entry(sg_policy, &attr_set->policy_list, tunables_hook) {
 		sg_policy->down_rate_delay_ns = rate_limit_us * NSEC_PER_USEC;
