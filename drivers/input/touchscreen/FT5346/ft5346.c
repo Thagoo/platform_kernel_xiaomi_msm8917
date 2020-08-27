8e643e438aa7d291886b2f6bd5fa8246a6b4f0dd
Anik1199 <anik@lineageos.org>
2020-08-27 13:30:12 +0530

    drivers: input: touchscreen:Create shared procfs nodes
    * Creates sysfs nodes and procfs symlinks to it to easy handle
    disable_keys functions from userspaceCreate shared procfs nodes
    
    * Update paths for rolex by Thago
    
    Signed-off-by: Thagoo <lohitgowda56@gmail.com>

--- drivers/input/touchscreen/FT5346/ft5346.c
+++ drivers/input/touchscreen/FT5346/ft5346.c
@@ -54,6 +54,12 @@ unsigned short coordinate_y[150] = {0};
 #include <linux/power_supply.h>
 #endif
 
+static ssize_t ft5x06_ts_disable_keys_show(struct device *dev,
+	struct device_attribute *attr, char *buf);
+
+static ssize_t ft5x06_ts_disable_keys_store(struct device *dev,
+struct device_attribute *attr, const char *buf, size_t count);
+
 #if defined(CONFIG_FB)
 #include <linux/notifier.h>
 #include <linux/fb.h>
@@ -508,7 +514,10 @@ static irqreturn_t ft5x06_ts_interrupt(int irq, void *dev_id)
 		/* invalid combination */
 		if (!num_touches && !status && !id)
 			break;
-
+			
+        if (data->disable_keys)
+            break;
+            
 		input_mt_slot(ip_dev, id);
 		if (status == FT_TOUCH_DOWN || status == FT_TOUCH_CONTACT) {
 			input_mt_report_slot_state(ip_dev, MT_TOOL_FINGER, 1);
@@ -908,9 +917,7 @@ static int fb_notifier_callback(struct notifier_block *self,
 	if (evdata && evdata->data && event == FB_EVENT_BLANK &&
 		ft5x06_data && ft5x06_data->client) {
 		blank = evdata->data;
-		if (*blank == FB_BLANK_UNBLANK
-                                || *blank == FB_BLANK_NORMAL
-                                || *blank == FB_BLANK_VSYNC_SUSPEND)
+		if (*blank == FB_BLANK_UNBLANK)
 		 schedule_work(&ft5x06_data->fb_notify_work);
 		else if (*blank == FB_BLANK_POWERDOWN) {
 			flush_work(&ft5x06_data->fb_notify_work);
@@ -2232,6 +2239,53 @@ static int ft5x06_parse_dt(struct device *dev,
 }
 #endif
 
+static DEVICE_ATTR(disable_keys, S_IWUSR | S_IRUSR, ft5x06_ts_disable_keys_show,
+		   ft5x06_ts_disable_keys_store);
+
+static struct attribute *ft5x06_ts_attrs[] = {
+    &dev_attr_disable_keys.attr,
+	NULL
+};
+
+static const struct attribute_group ft5x06_ts_attr_group = {
+	.attrs = ft5x06_ts_attrs,
+};
+
+static int ft5x06_proc_init(struct ft5x06_ts_data *data)
+{
+       struct i2c_client *client = data->client;
+
+       int ret = 0;
+       char *buf, *path = NULL;
+       char *key_disabler_sysfs_node;
+       struct proc_dir_entry *proc_entry_tp = NULL;
+       struct proc_dir_entry *proc_symlink_tmp = NULL;
+
+       buf = kzalloc(sizeof(struct ft5x06_ts_data), GFP_KERNEL);
+       if (buf)
+               path = "/devices/soc/78b7000.i2c/i2c-3/3-003e";
+
+       proc_entry_tp = proc_mkdir("touchpanel", NULL);
+       if (proc_entry_tp == NULL) {
+               dev_err(&client->dev, "Couldn't create touchpanel dir in procfs\n");
+               ret = -ENOMEM;
+       }
+
+       key_disabler_sysfs_node = kzalloc(sizeof(struct ft5x06_ts_data), GFP_KERNEL);
+       if (key_disabler_sysfs_node)
+               sprintf(key_disabler_sysfs_node, "/sys%s/%s", path, "disable_keys");
+       proc_symlink_tmp = proc_symlink("capacitive_keys_enable",
+                       proc_entry_tp, key_disabler_sysfs_node);
+       if (proc_symlink_tmp == NULL) {
+               dev_err(&client->dev, "Couldn't create capacitive_keys_enable symlink\n");
+               ret = -ENOMEM;
+       }
+
+       kfree(buf);
+       kfree(key_disabler_sysfs_node);
+       return ret;
+}
+
 
 #ifdef CONFIG_TOUCHSCREEN_LOCKDOWN_INFO
 static ssize_t ctp_lockdown_proc_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
@@ -3151,7 +3205,14 @@ static int ft5x06_ts_probe(struct i2c_client *client,
 	if (!batt_psy)
 		CTP_DEBUG("tp battery supply not found\n");
 #endif
-
+   err = sysfs_create_group(&client->dev.kobj, &ft5x06_ts_attr_group);
+   if (err) {
+		dev_err(&client->dev, "Failure %d creating sysfs group\n",
+			err);
+		goto free_reset_gpio;
+        }
+        
+    ft5x06_proc_init(data);
 	enable_irq(data->client->irq);
 
 	return 0;
@@ -3220,6 +3281,7 @@ static int ft5x06_ts_remove(struct i2c_client *client)
 		if (retval < 0)
 			CTP_ERROR("Cannot get idle pinctrl state\n");
 	}
+	sysfs_remove_group(&client->dev.kobj, &ft5x06_ts_attr_group);
 	input_unregister_device(data->input_dev);
 
 	return 0;
@@ -3248,6 +3310,29 @@ void ft5x06_ts_shutdown(struct i2c_client *client)
 		data->pdata->power_init(false);
 	else
 		ft5x06_power_init(data, false);
+}
+
+static ssize_t ft5x06_ts_disable_keys_show(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	struct ft5x06_ts_data *data = dev_get_drvdata(dev);
+	const char c = data->disable_keys ? '1' : '0';
+	return sprintf(buf, "%c\n", c);
+}
+
+static ssize_t ft5x06_ts_disable_keys_store(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct ft5x06_ts_data *data = dev_get_drvdata(dev);
+	int i;
+
+	if (sscanf(buf, "%u", &i) == 1 && i < 2) {
+		data->disable_keys = (i == 1);
+		return count;
+	} else {
+		dev_dbg(dev, "disable_keys write error\n");
+		return -EINVAL;
+	}
 }
 
 
