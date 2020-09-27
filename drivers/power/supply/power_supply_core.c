a04db60ba5b05090a0b5e55f9445aeea7e9af76e
Lanik <daniilt971@gmail.com>
2020-09-27 21:52:09 +0300

    drivers: Import Xiaomi power changes
    Signed-off-by: Thagoo <lohitgowda56@gmail.com>

--- drivers/power/supply/power_supply_core.c
+++ drivers/power/supply/power_supply_core.c
@@ -59,6 +59,22 @@ static bool __power_supply_is_supplied_by(struct power_supply *supplier,
 	return false;
 }
 
+int power_supply_get_battery_charge_state(struct power_supply *psy)
+{
+	union power_supply_propval ret = {0,};
+	if (!psy) {
+		 pr_err("power supply is NULL\n");
+	}
+
+	if (psy->desc->get_property) 
+		psy->desc->get_property(psy, POWER_SUPPLY_PROP_PRESENT, &ret);
+		
+	pr_debug("online:%d\n", ret.intval);
+
+	return ret.intval;
+}
+EXPORT_SYMBOL(power_supply_get_battery_charge_state);
+
 static int __power_supply_changed_work(struct device *dev, void *data)
 {
 	struct power_supply *psy = data;
