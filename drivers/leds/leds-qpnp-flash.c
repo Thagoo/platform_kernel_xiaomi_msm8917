redmidevs/android_kernel_xiaomi_msm8917

drivers: leds: Import Xiaomi leds-qpnp-flash changes
Change-Id: I9cad5cae5ad7af3aa47c7c6dd7c705c0c5889bac

drivers/leds/leds-qpnp-flash.c
@@ -1,4 +1,5 @@
 /* Copyright (c) 2014-2017, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2018 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -29,6 +30,7 @@
 #include <linux/leds-qpnp-flash.h>
 #include <linux/debugfs.h>
 #include <linux/uaccess.h>
+#include <linux/wakelock.h>
 #include "leds.h"
 
 #define FLASH_LED_PERIPHERAL_SUBTYPE(base)			(base + 0x05)
@@ -249,6 +251,8 @@ struct qpnp_flash_led {
 	struct workqueue_struct		*ordered_workq;
 	struct qpnp_vadc_chip		*vadc_dev;
 	struct mutex			flash_led_lock;
+	struct wake_lock		flashlight_led_lock;
+	struct qpnp_flash_led_buffer	*log;
 	struct dentry			*dbgfs_root;
 	int				num_leds;
 	u16				base;
@@ -1311,6 +1315,8 @@ static void qpnp_flash_led_work(struct work_struct *work)
 
 	/* Global lock is to synchronize between the flash leds and torch */
 	mutex_lock(&led->flash_led_lock);
+
+	dev_dbg(&led->spmi_dev->dev, "wt flash_node.cdev.name=%s\n", flash_node->cdev.name);
 	/* Local lock is to synchronize for one led instance */
 	mutex_lock(&flash_node->cdev.led_access);
 
@@ -1866,6 +1872,7 @@ error_enable_gpio:
 	return;
 }
 
+extern  int32_t wt_flash_flashlight(bool boolean);
 static void qpnp_flash_led_brightness_set(struct led_classdev *led_cdev,
 						enum led_brightness value)
 {
@@ -1883,6 +1890,21 @@ static void qpnp_flash_led_brightness_set(struct led_classdev *led_cdev,
 		value = flash_node->cdev.max_brightness;
 
 	flash_node->cdev.brightness = value;
+
+	pr_debug("WT flash_node.cdev.name=%s, brightness=%d, id=%d, flash_node->type=%d\n", flash_node->cdev.name,
+		       flash_node->cdev.brightness, flash_node->id, flash_node->type);
+
+	if (!strcmp(flash_node->cdev.name, "flashlight")) {
+		pr_info("wt_flash_flashlight  enter value=%d\n", value);
+		if (value > 0) {
+			wt_flash_flashlight(true);
+			wake_lock(&led->flashlight_led_lock);
+		} else{
+			wt_flash_flashlight(false);
+			wake_unlock(&led->flashlight_led_lock);
+		}
+	}
+
 	if (led->flash_node[led->num_leds - 1].id ==
 						FLASH_LED_SWITCH) {
 		if (flash_node->type == TORCH)
@@ -2571,6 +2593,8 @@ static int qpnp_flash_led_probe(struct spmi_device *spmi)
 	}
 
 	mutex_init(&led->flash_led_lock);
+	wake_lock_init(&led->flashlight_led_lock, WAKE_LOCK_SUSPEND,
+			"flashlight_led_lock_wt");
 
 	led->ordered_workq = alloc_ordered_workqueue("flash_led_workqueue", 0);
 	if (!led->ordered_workq) {
@@ -2710,6 +2734,7 @@ error_led_register:
 		led_classdev_unregister(&led->flash_node[i].cdev);
 	}
 	mutex_destroy(&led->flash_led_lock);
+	wake_lock_destroy(&led->flashlight_led_lock);
 	destroy_workqueue(led->ordered_workq);
 
 	return rc;
@@ -2735,6 +2760,7 @@ static int qpnp_flash_led_remove(struct spmi_device *spmi)
 	}
 	debugfs_remove_recursive(led->dbgfs_root);
 	mutex_destroy(&led->flash_led_lock);
+	wake_lock_destroy(&led->flashlight_led_lock);
 	destroy_workqueue(led->ordered_workq);
 
 	return 0;

Thagoo/platform_kernel_xiaomi_msm8917

Signed-off-by: Thagoo <lohitgowda56@gmail.com>

drivers/leds/leds-qpnp-flash.c
@@ -1,4 +1,5 @@
 /* Copyright (c) 2014-2018, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2018 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -31,6 +32,7 @@
 #include <linux/qpnp/qpnp-revid.h>
 #include <linux/debugfs.h>
 #include <linux/uaccess.h>
+#include <linux/wakelock.h>
 #include "leds.h"
 
 #define FLASH_LED_PERIPHERAL_SUBTYPE(base)			(base + 0x05)
@@ -251,6 +253,8 @@ struct qpnp_flash_led {
 	struct workqueue_struct		*ordered_workq;
 	struct qpnp_vadc_chip		*vadc_dev;
 	struct mutex			flash_led_lock;
+	struct wake_lock		flashlight_led_lock;
+	struct qpnp_flash_led_buffer	*log;
 	struct dentry			*dbgfs_root;
 	int				num_leds;
 	u16				base;
@@ -1274,6 +1278,8 @@ static void qpnp_flash_led_work(struct work_struct *work)
 
 	mutex_lock(&led->flash_led_lock);
 
+	dev_dbg(&led->pdev->dev, "wt flash_node.cdev.name=%s\n", flash_node->cdev.name);
+
 	if (!brightness)
 		goto turn_off;
 
@@ -1825,6 +1831,7 @@ error_enable_gpio:
 	mutex_unlock(&led->flash_led_lock);
 }
 
+extern  int32_t wt_flash_flashlight(bool boolean);
 static void qpnp_flash_led_brightness_set(struct led_classdev *led_cdev,
 						enum led_brightness value)
 {
@@ -1843,6 +1850,21 @@ static void qpnp_flash_led_brightness_set(struct led_classdev *led_cdev,
 		value = flash_node->cdev.max_brightness;
 
 	flash_node->cdev.brightness = value;
+
+	pr_debug("WT flash_node.cdev.name=%s, brightness=%d, id=%d, flash_node->type=%d\n", flash_node->cdev.name,
+		       flash_node->cdev.brightness, flash_node->id, flash_node->type);
+
+	if (!strcmp(flash_node->cdev.name, "flashlight")) {
+		pr_info("wt_flash_flashlight  enter value=%d\n", value);
+		if (value > 0) {
+			wt_flash_flashlight(true);
+			wake_lock(&led->flashlight_led_lock);
+		} else{
+			wt_flash_flashlight(false);
+			wake_unlock(&led->flashlight_led_lock);
+		}
+	}
+
 	if (led->flash_node[led->num_leds - 1].id ==
 						FLASH_LED_SWITCH) {
 		if (flash_node->type == TORCH)
@@ -2513,6 +2535,8 @@ static int qpnp_flash_led_probe(struct platform_device *pdev)
 	}
 
 	mutex_init(&led->flash_led_lock);
+	wake_lock_init(&led->flashlight_led_lock, WAKE_LOCK_SUSPEND,
+			"flashlight_led_lock_wt");
 
 	led->ordered_workq = alloc_ordered_workqueue("flash_led_workqueue", 0);
 	if (!led->ordered_workq) {
@@ -2650,6 +2674,7 @@ error_led_register:
 		led_classdev_unregister(&led->flash_node[i].cdev);
 	}
 	mutex_destroy(&led->flash_led_lock);
+	wake_lock_destroy(&led->flashlight_led_lock);
 	destroy_workqueue(led->ordered_workq);
 
 	return rc;
@@ -2675,6 +2700,7 @@ static int qpnp_flash_led_remove(struct platform_device *pdev)
 	}
 	debugfs_remove_recursive(led->dbgfs_root);
 	mutex_destroy(&led->flash_led_lock);
+	wake_lock_destroy(&led->flashlight_led_lock);
 	destroy_workqueue(led->ordered_workq);
 
 	return 0;