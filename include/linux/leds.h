327da8328be1a4e16d8212d3f36bc4bf5afc26be
Thagoo <lohitgowda56@gmail.com>
2020-11-02 10:08:40 +0000

    Revert "leds: Bringback 3.18 gpio_led_platform_data"
    This reverts commit b3219d2882b8b1de99388626f7ef3ce9183acfbd.

--- include/linux/leds.h
+++ include/linux/leds.h
@@ -389,9 +389,7 @@ struct gpio_led_platform_data {
 #define GPIO_LED_NO_BLINK_LOW	0	/* No blink GPIO state low */
 #define GPIO_LED_NO_BLINK_HIGH	1	/* No blink GPIO state high */
 #define GPIO_LED_BLINK		2	/* Please, blink */
-	int		(*gpio_blink_set)(unsigned gpio, int state,
-					unsigned long *delay_on,
-					unsigned long *delay_off);
+	gpio_blink_set_t	gpio_blink_set;
 };
 
 #ifdef CONFIG_NEW_LEDS
