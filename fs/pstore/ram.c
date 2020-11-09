edccc8a3649f23f0aad46c44d3c6277349b8b4f7
Thagoo <lohitgowda56@gmail.com>
2020-11-09 15:30:40 +0000

    fix ramoops format

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -819,8 +819,8 @@ static int __init ramoops_memreserve(char *p)
 	ramoops_data.pmsg_size = size / 2;
 	ramoops_data.dump_oops = 1;
 
-	pr_info("msm_reserve_ramoops_memory addr=%lx,size=%lx\n",
-			ramoops_data.mem_address, ramoops_data.mem_size);
+	pr_info("msm_reserve_ramoops_memory addr=%lx, size=%lx\n",
+			(unsigned long)ramoops_data.mem_address, ramoops_data.mem_size);
 	pr_info("msm_reserve_ramoops_memory record_size=%lx,ftrace_size=%lx\n",
 			ramoops_data.record_size, ramoops_data.ftrace_size);
 
